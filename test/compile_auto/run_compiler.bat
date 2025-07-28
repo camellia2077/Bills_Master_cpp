15a1042cd80197f33193921503b9c2bd731aa926
camellia <130271834+camellia2077@users.noreply.github.com>
2025-07-28 16:03:16 +0800

    拆分

--- test/compile_auto/run_compiler.bat
+++ test/compile_auto/run_compiler.bat
@@ -5,6 +5,5 @@ REM 设置硬编码路径
 set "TARGET_PATH=C:\Computer\my_github\github_cpp\bill_master\Bills_Master_cpp\my_test\exported_files"
 
 REM 执行 Python 脚本并传入参数
-python compiler.py auto "%TARGET_PATH%"
+python main.py auto "%TARGET_PATH%"
 
-pause
