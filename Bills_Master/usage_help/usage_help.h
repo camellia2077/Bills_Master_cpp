d2fea362ddbd73af0d56beaa40d354c3017a7e87
camellia <130271834+camellia2077@users.noreply.github.com>
2025-07-29 21:44:36 +0800

    打印帮助提取为模块

---
+++ Bills_Master/usage_help/usage_help.h
@@ -0,0 +1,10 @@
+#ifndef USAGE_HELP_H
+#define USAGE_HELP_H
+
+/**
+ * @brief Prints the command-line help message.
+ * * @param program_name The name of the executable, typically argv[0].
+ */
+void print_help(const char* program_name);
+
+#endif // USAGE_HELP_H
